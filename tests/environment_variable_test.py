dfd94500df4387e63ab569ea25f6c2914727bf53
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-06 15:45:14 -0400

    Task: Tests to see if the application is in the environment variable of testing

--- tests/environment_variable_test.py
+++ tests/environment_variable_test.py
@@ -11,3 +11,11 @@ def test_environment_variable_development(application):
     assert application.config['DEBUG']
     assert not application.config['TESTING']
 
+
+def test_environment_variable_testing(application):
+    application.config.from_object('app.config.TestingConfig')
+    assert application.config['DEBUG']
+    assert application.config['TESTING']
+    assert not application.config['PRESERVE_CONTEXT_ON_EXCEPTION']
+    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///:memory:'
+
