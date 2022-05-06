906f0304f4bbbbd42c3a00dc4367d0e1a5f7f7dc
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-06 15:25:49 -0400

    Task: Tests to see if the copyright is printed in the footer

--- tests/context_process_test.py
+++ tests/context_process_test.py
@@ -11,3 +11,12 @@ def test_context_variables_environment(client):
     content = bytes(test_string, 'utf-8')
     assert response.status_code == 200
     assert content in response.data
+
+
+def test_context_variables_copyright(client):
+    """This tests checks if the copyright is printed"""
+    response = client.get("/")
+    test_string = f"Copyright: "
+    content = bytes(test_string, 'utf-8')
+    assert response.status_code == 200
+    assert content in response.data
