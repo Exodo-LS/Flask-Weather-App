d04fe78cfa99725ba2d13e4ebaa307d5e90f901f
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-06 16:19:14 -0400

    Task: Testing The Index Page Exists

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -10,5 +10,11 @@ def test_nav_bar_simple_pages_links(client):
     assert b'href="/welcome"' in response.data
 
 
+def test_index_page_exists(client):
+    """This validates the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Index" in response.data
+
 
 
