e19b9d9777a44551d0db7b843866c839da1b456c
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-06 21:56:29 -0400

    Task: Validating the access to the forecast page

--- tests/forecast_test.py
+++ tests/forecast_test.py
@@ -8,3 +8,14 @@ def test_forecast_page_exists(client):
     response = client.get("/temperature_forecast")
     assert response.status_code == 405
 
+
+def test_forecast_page_access(client):
+    """This validates access to the forecast page"""
+    response = client.get("/temperature_forecast")
+    test_user = 'IS219_TestUser@email.com'
+    test_password = 'Dummy_Pass_123'
+    if User.email == test_user and User.password == test_password:
+        if User.authenticated:
+            assert 'User Authenticated' in response.data
+    elif not User.authenticated:
+        assert 'User Not Authenticated' in response.data
